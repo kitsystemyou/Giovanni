a3ff7350569a51232f3a4c8a7cf24a2ed7a51188
kitsystemyou <kitsystemyou@gmail.com>
2023-06-28 03:17:58 +0900

    Fix res of get document

--- fire_store.py
+++ fire_store.py
@@ -8,10 +8,12 @@ def get_documents(user_id: str, group_id: str):
     collections: list[firestore_v1.CollectionReference] = db.collection(user_id).document(group_id).collections(timeout=5)
     print("got collections")
     docs: dict = {}
+    set_list: list = []
     for c in collections:
-        for doc in c.stream(timeout=5):
-            print(f'{doc.id}')
-            docs.update({c.id: {doc.id: doc.to_dict()}})
-    print(len(docs))
+        for doc_snp in c.stream(timeout=5):
+            doc = doc_snp.to_dict()
+            doc.update({"id": c.id})
+            set_list.append(doc)
     print(f'user_id: {user_id}, group_id: {group_id}')
+    docs.update({"result": set_list})
     return docs
