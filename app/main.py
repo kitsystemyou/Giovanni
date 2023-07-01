1e77da2b9c27e44fc73cc43f2f8b84949bf7ae58
kitsystemyou <kitsystemyou@gmail.com>
2023-07-02 03:49:05 +0900

    Refine Code

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
-from typing import Annotated
-
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 
 from cloud_storage import authenticate_implicit_with_adc
@@ -28,11 +26,6 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/files")
-async def create_file(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
-
-
 @app.post("/upload/{user_id}/{group_id}")
 async def create_upload_file(file: UploadFile, user_id: str, group_id: str):
     print("filename:", file.filename)
@@ -45,8 +38,3 @@ async def get_items(user_id, group_id: str):
     dict_docs = get_documents(user_id, group_id)
     return dict_docs
 
-
-# Snippet
-@app.get("/items/{item_id}")
-async def read_item(item_id):
-    return {"item_id": item_id}
